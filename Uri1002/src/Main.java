6a7a0b35e8439ab852030cca7c4b3da24aee940e
unknown <Wesleyscorrea@hotmail.com>
2022-04-07 19:35:17 -0300

    Adicionado pasta Uri1002

---
+++ Uri1002/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+				
+		double R , A ;
+			
+		R = sc.nextDouble();
+				
+		A = 3.14159 * R * R;
+		
+		System.out.printf("A=%.4f%n" , A);
+		
+		sc.close();
+	}
+}
